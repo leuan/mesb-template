2a2ecf61a4854c8f7e2e8f5e8823d7caa0ab0787
Leon Vintzeanu <leonvintzeanu@protonmail.com>
2023-09-18 20:18:51 +0300

    added tailwind support

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,9 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to slay the documentation</p>
+<p class="text-2xl font-bold">Visit <a  href="https://kit.svelte.dev">kit.svelte.dev</a> to slay the documentation</p>
+<style lang="postcss">
+    p {
+        @apply text-purple-800;
+    }
+
+
+</style>
